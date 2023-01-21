e7001f7ce147f404c4eecf6d32fed1fef08e4191
Kitamura <hinitial@gmail.com>
2023-01-21 20:22:37 +0900

    jest typescript 準備

--- pages/index.vue
+++ pages/index.vue
@@ -11,9 +11,11 @@
 <script lang="ts">
 import { defineComponent, ref, inject } from '@nuxtjs/composition-api'
 import { UseModal, UseModalKey } from '@/composables/useModal'
-
+import RoundNum from '@/components/RoundNum.vue'
+import Modal from '@/components/Modal.vue'
 export default defineComponent({
   name: 'IndexPage',
+  components: { RoundNum, Modal },
   setup() {
     const { open: openModal, close: closeModal, openDialog } = inject(UseModalKey) as UseModal
     const openDialogInLocal = async () => {
--- test/PageIndex.test.ts
+++ test/PageIndex.test.ts
@@ -1,3 +1,9 @@
-test('check', () => {
-  console.log('OK')
+import { mount } from '@vue/test-utils'
+import PageIndex from '@/pages/index.vue'
+
+describe('PageIndex', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(PageIndex)
+    expect(wrapper.vm).toBeTruthy()
+  })
 })
